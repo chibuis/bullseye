edec0554d9ec5d9a316c25b18537faa1cfc79cfe
chibuis <150316538+chibuis@users.noreply.github.com>
2024-05-30 07:38:39 +0100

    initial

--- src/pages/loginpage.js
+++ src/pages/loginpage.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 function Login(){
-
-  return(
+  const [email,]
+    return(
     <div className="login">
       <h1>Login</h1>
       <form action="POST">
