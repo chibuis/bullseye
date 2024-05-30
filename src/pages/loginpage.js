4e4514250ad1c8128c629bbf09aab0d8006aae13
chibuis <150316538+chibuis@users.noreply.github.com>
2024-05-30 07:42:31 +0100

    login

--- src/pages/loginpage.js
+++ src/pages/loginpage.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 function Login(){
-  const [email,]
+  const [email,setEmail] = useState('')
+  const [password,setPassword] = useState('')
     return(
     <div className="login">
       <h1>Login</h1>
